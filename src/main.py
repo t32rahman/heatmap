c03f60ddcfe0a2456b91851f5c280ce47402854c
t32rahman <t10rahman@ryerson.ca>
2020-09-02 08:26:13 -0400

    Added comments

--- src/main.py
+++ src/main.py
@@ -2,38 +2,45 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-# Initial Setup
+# Initial Setup ------------------------------------------
+PAUSE = 0.5 # Pause inbetween refreshes (Seconds)
 radiation = pd.read_json("input.json")
 
-plt.ion()
+plt.ion() #Interactive on
 
+# Absolute axis
 ax = plt.gca()
 ax.ticklabel_format(useOffset=False)
 
+# Labels
 title = plt.title("Rover Radiation Graph")
-
 plt.xlabel("Latitude")
 plt.ylabel("Longitude")
+
+# Create Plot
 plt.scatter("lat", "long", c='b', data=radiation)
 plt.colorbar()
 plt.draw()
-plt.pause(0.5)
+plt.pause(PAUSE)
 
-
+# Main Loop ----------------------------------------------
 while True:
+    # Pretty much Same as initial setup
+    # Except this loop doesn't add color bar
     radiation = pd.read_json("input.json")
+
     ax = plt.gca()
     ax.ticklabel_format(useOffset=False)
-    title = plt.title("Rover Radiation Graph")
 
+    title = plt.title("Rover Radiation Graph")
     plt.xlabel("Latitude")
     plt.ylabel("Longitude")
 
     plt.scatter("lat", "long", c='b', data=radiation)
     plt.draw()
-    plt.pause(0.5)
+    plt.pause(PAUSE)
     
 
 print("Ok")
-# plt.draw()
+
 
